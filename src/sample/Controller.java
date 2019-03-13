ac3dc69f1449293ebc3bda7b433280813b10290d
Dmitry <dmitry@unipat.ru>
2019-03-13 05:51:00 +0300

    Lesson 4: Answer (Swing + JavaFX + DopDZ)

---
+++ src/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    @FXML
+    Button btn;
+
+    public void sendMsg() {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+
+}
